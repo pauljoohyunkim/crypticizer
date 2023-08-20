c9d03a77b082ac72e8c8e1cca8cdaa25dfa0add7
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-08-20 20:44:23 +0900

    smart_git_tree_lookup not tested

--- src/gitwrap.cpp
+++ src/gitwrap.cpp
@@ -6,6 +6,7 @@
 static void smart_git_repository_free(git_repository* repo);
 static void smart_git_index_free(git_index* idx);
 static void smart_git_signature_free(git_signature* signature);
+static void smart_git_tree_free(git_tree* tree);
 
 /* Open git repository */
 SMART_GIT_WRAP(git_repository) smart_git_repository_open(std::string repoPath)
@@ -40,11 +41,17 @@ SMART_GIT_WRAP(git_signature) smart_git_signature_default(SMART_GIT_WRAP(git_rep
     auto smart_signature { SMART_GIT_WRAP(git_signature)(signature, smart_git_signature_free) };
     return smart_signature;
 }
+SMART_GIT_WRAP(git_tree) smart_git_tree_lookup(SMART_GIT_WRAP(git_repository) repo, git_oid oid)
+{
+    git_tree* tree;
+    auto error { git_tree_lookup(&tree, repo.get(), &oid) };
+
+    auto smart_tree { SMART_GIT_WRAP(git_tree)(tree, smart_git_tree_free) };
+    return smart_tree;
+}
 
 
-//void smart_commit(
-//    std::unique_ptr<git_repository, void (*)(git_repository *)> &repo,
-//    std::unique_ptr<git_index, void (*)(git_index *)> &index)
+//void smart_commit(SMART_GIT_WRAP(git_repository) &repo, SMART_GIT_WRAP(git_index) &index)
 //{
 //
 //}
@@ -65,4 +72,8 @@ static void smart_git_index_free(git_index* idx)
 static void smart_git_signature_free(git_signature* signature)
 {
     git_signature_free(signature);
+}
+static void smart_git_tree_free(git_tree* tree)
+{
+    git_tree_free(tree);
 }
