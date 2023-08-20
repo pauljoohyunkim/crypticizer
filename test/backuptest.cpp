fce83131387c0f91a45e29646e17f500e0f4e8ae
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-08-20 17:02:47 +0900

    backuptest repo with directory

--- test/backuptest.cpp
+++ test/backuptest.cpp
@@ -10,7 +10,11 @@ int main()
         auto repo = smart_git_repository_open("temprepo");
         auto idx = smart_git_repository_index(repo);
 
-        smart_git_index_add_by_path(idx, "testfile");
+        smart_git_index_add_by_path(idx, "testfile1");
+        smart_git_index_add_by_path(idx, "testfile2");
+        smart_git_index_add_by_path(idx, "testfile3");
+        smart_git_index_add_by_path(idx, "repodir/testfile4");
+
     }
 
     git_libgit2_shutdown();
