4fda356a500652587c0c649be2e033ae9d5f84a4
pauljoohyunkim <pauljoohyunkim@gmail.com>
2023-08-21 23:10:07 +0900

    Abstracted away the staging process

--- src/backup.cpp
+++ src/backup.cpp
@@ -3,3 +3,29 @@
 #include <memory>
 #include "backup.h"
 
+Backupper::Backupper(std::string sessionPath)
+{
+    git_libgit2_init();
+    repo = smart_git_repository_open(sessionPath);
+    smart_git_revparse_ext(repo, parent, ref);
+    index = smart_git_repository_index(repo);
+    signature = smart_git_signature_default(repo);
+}
+
+Backupper::~Backupper()
+{
+    git_libgit2_shutdown();
+}
+
+void Backupper::stageFile(std::string filePath)
+{
+    smart_git_index_add_by_path(index, filePath);
+}
+
+void Backupper::stageFiles(std::vector<std::string> filePath_v)
+{
+    for (auto filePath : filePath_v)
+    {
+        stageFile(filePath);
+    }
+}
